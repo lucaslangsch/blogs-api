5d7f441fc737ae31c05f64275e72d1c3337733c9
lucaslangsch <lucas.langsch@gmail.com>
2023-08-03 16:02:28 -0300

    feat: create model PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+      postId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      categoryId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'post_categories',
+      underscored: true,
+      timestamps: false,
+    },
+  );
+
+  PostCategory.associate = ({ Category, BlogPost }) => {
+    BlogPost.belongsToMany(Category, {
+      as: 'categories',
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+      through: PostCategory,
+    });
+    
+    Category.belongsToMany(BlogPost, {
+      as: 'posts',
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      through: PostCategory,
+    });
+  };
+
+  return PostCategory;
+};
